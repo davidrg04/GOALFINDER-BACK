fdf3173958d0b214a13bd083626119068fc44b97
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2024-05-22 21:59:53 +0200

    CORS

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'c31fcd75985d33ae77a9bf34986e146e64cc0941',
+        'reference' => '899f0e7de6feac1d2449a919961f9f96456a27f6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'c31fcd75985d33ae77a9bf34986e146e64cc0941',
+            'reference' => '899f0e7de6feac1d2449a919961f9f96456a27f6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
