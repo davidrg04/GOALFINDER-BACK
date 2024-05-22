f56c9f58cb8fdf7af2bd38335524341174056513
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2024-05-22 18:09:49 +0200

    CORS

--- API/ACCESS/login.php
+++ API/ACCESS/login.php
@@ -4,21 +4,17 @@ require_once('../CONEXION/conexion.php');
 $con = new Conexion();
 $db = $con->getDb();
 
+// Encabezados CORS
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // Cache por 1 día
 }
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
+// Manejar solicitudes OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-    }
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
     http_response_code(200);
     exit();
 }
