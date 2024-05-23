1d7564f17bacfe64419568a43d81fb87bda37465
David Rodríguez Gallego <127893741+davidrg04@users.noreply.github.com>
2024-05-23 13:03:29 +0200

    Create install_mongodb.sh

---
+++ install_mongodb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Actualizar el paquete e instalar las dependencias necesarias
+apt-get update && apt-get install -y php-pear php8.2-dev && pecl install mongodb && echo "extension=mongodb.so" > /etc/php/8.2/cli/conf.d/20-mongodb.ini
+
+# Instalar las dependencias de Composer
+composer install --ignore-platform-reqs
