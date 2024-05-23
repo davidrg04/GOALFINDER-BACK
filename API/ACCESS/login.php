cf87d864b17efb55ef38320ae53161a9bc3951b3
David Rodríguez Gallego <127893741+davidrg04@users.noreply.github.com>
2024-05-23 11:46:39 +0200

    Update login.php

--- API/ACCESS/login.php
+++ API/ACCESS/login.php
@@ -1,21 +1,19 @@
 <?php
 require_once('../CONEXION/conexion.php');
 
-$con = new Conexion();
-$db = $con->getDb();
+header("Access-Control-Allow-Origin: https://goalfinder-front.vercel.app");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: *");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Content-Length: 0");
     header("HTTP/1.1 200 OK");
     exit;
 }
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+$con = new Conexion();
+$db = $con->getDb();
 
 $data = json_decode(file_get_contents("php://input"), true);
  
