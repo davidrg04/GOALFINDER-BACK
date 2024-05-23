d33659849129e97034d5da29966de365e16aedb7
David Rodríguez Gallego <127893741+davidrg04@users.noreply.github.com>
2024-05-23 13:38:45 +0200

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+add-apt-repository ppa:ondrej/php -y
 # Actualizar el paquete e instalar las dependencias necesarias
 apt-get update && apt-get install -y php-pear php8.2-dev && pecl install mongodb && echo "extension=mongodb.so" > /etc/php/8.2/cli/conf.d/20-mongodb.ini
 
